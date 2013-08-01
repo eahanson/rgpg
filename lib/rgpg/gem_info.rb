00a2b6854bb3f602fe39d869ccdcb94198240a92
Richard Cook <rcook@rcook.org>
2013-07-31 20:45:09 -0700

    Bump patch number

--- lib/rgpg/gem_info.rb
+++ lib/rgpg/gem_info.rb
@@ -2,7 +2,7 @@ module Rgpg
   module GemInfo
     MAJOR_VERSION = 0
     MINOR_VERSION = 2
-    PATCH_VERSION = 1
+    PATCH_VERSION = 2
 
     def self.version_string
       [MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION].join('.')
